9f58491b96a1cc364c9498dea49dcbbcca368ca3
Tang Lam <lam.tang@mobi.ch>
2018-08-31 18:09:03 +0700

    add router-store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 import {environment} from '../environments/environment';
 import {EffectsModule} from '@ngrx/effects';
 import {CurrentConditionsEffects} from './effects/current-conditions.effects';
+import {StoreRouterConnectingModule} from '@ngrx/router-store';
 
 @NgModule({
     declarations: [
@@ -34,6 +35,7 @@ import {CurrentConditionsEffects} from './effects/current-conditions.effects';
         RouterModule,
         routing,
         EffectsModule.forRoot([CurrentConditionsEffects]),
+        StoreRouterConnectingModule.forRoot({stateKey: 'router'}),
         StoreModule.forRoot(reducers, {metaReducers}),
         !environment.production ? StoreDevtoolsModule.instrument() : [],
     ],
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -2,15 +2,18 @@ import {ActionReducerMap, createSelector, MetaReducer} from '@ngrx/store';
 import {environment} from '../../environments/environment';
 import {zipCodeReducer, ZipcodesState} from './zip-codes.reducer';
 import {currentConditionsReducer, CurrentConditionsState} from './current-conditions.reducer';
+import {routerReducer, RouterReducerState} from '@ngrx/router-store';
 
 export interface State {
     zipCodes: ZipcodesState,
-    currentConditions: CurrentConditionsState
+    currentConditions: CurrentConditionsState,
+    router: RouterReducerState,
 }
 
 export const reducers: ActionReducerMap<State> = {
     zipCodes: zipCodeReducer,
     currentConditions: currentConditionsReducer,
+    router: routerReducer,
 };
 
 export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
